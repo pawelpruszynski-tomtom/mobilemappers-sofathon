6cb90c0e593f5efd1998adec2cbf9d7f2ec51875
gronski <Grzegorz.Gronski@tomtom.com>
2022-11-09 17:17:00 +0100

    multiple files upload php

--- upload.php
+++ upload.php
@@ -1,8 +1,10 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+
 $uploadOk = 1;
+
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -25,12 +27,24 @@ if (file_exists($target_file)) {
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
+                }
+else {
+
+     $total = count($_FILES['fileToUpload']['tmp_name']);
+
+     for( $i=0 ; $i < $total ; $i++ ) {
+
+               $tmpFilePath = $_FILES['fileToUpload']['tmp_name'][$i];
+
+               if ($tmpFilePath != ""){
+                         $newFilePath = "./uploads/" . $_FILES['fileToUpload']['tmp_name'][$i];
+                         if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+                                           echo "The file ". htmlspecialchars( basename( $_FILES['fileToUpload']['tmp_name'][$i])). " has been uploaded.";
+                                             }
+                                      else
+                                      { echo "Sorry, there was an error uploading your file."; }
+                                      }
+      }
+                }
+
 ?>
